15d5d8d3a943c65d5c5c23ec0520aa7ad4b04198
Mike Lawton <mike.lawton@mac.com>
2022-11-03 11:15:13 +0000

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,34 @@
 module.exports = {
   siteMetadata: {
     title: `Unity Mutual`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://wordpress-257636-3005210.cloudwaysapps.com`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-wordpress',
-    options: {
-      "url": "http://unity.mutual/graphql"
-    }
-  }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "https://wordpress-257636-3005210.cloudwaysapps.com/graphql",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
